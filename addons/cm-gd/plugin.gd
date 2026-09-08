bfb765067a33c231db997182ca9515c056216ca2
maji <maji@himaji.xyz>
2026-09-08 09:47:22 +0700

    Print welcome message on enable

--- addons/cm-gd/plugin.gd
+++ addons/cm-gd/plugin.gd
@@ -4,7 +4,16 @@ extends EditorPlugin
 var splash_inspector_plugin: EditorInspectorPlugin
 
 func _enable_plugin() -> void:
-	pass
+	# Print welcome message
+	print_rich("————————————————————————————————————————————\n")
+	print_rich("[img=0x35]res://addons/cm-gd/editor/assets/cm_logo.png[/img]")
+	print_rich("[font_size=20]Welcome to CM.gd![/font_size]")
+	print_rich("Thank you for checking out CM.gd, here are some of the useful resources you can check out!\n")
+	print_rich("↗ Documentation [url]https://cmgd.dev/[/url]")
+	print_rich("↗ Discord [url]https://discord.gg/KrqmuQzxKK[/url]")
+	print_rich("↗ Report Issues [url]https://github.com/maji-git/cm-gd/issues[/url]")
+	print_rich("\nHappy Developing! 🧡")
+	print_rich("\n————————————————————————————————————————————")
 
 func _disable_plugin() -> void:
 	pass
