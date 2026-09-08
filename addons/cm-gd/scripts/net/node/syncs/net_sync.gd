bd6a0c33484202ad6351571bc991618c79d88474
maji <maji@himaji.xyz>
2026-09-08 07:37:33 +0700

    Move `can_take_authority` to global

--- addons/cm-gd/scripts/net/node/syncs/net_sync.gd
+++ addons/cm-gd/scripts/net/node/syncs/net_sync.gd
@@ -4,3 +4,8 @@ enum NetSyncAuthorityMode {
 	SINGLE_AUTHORITY,
 	SHARED
 }
+
+static func can_take_authority(from_id: int, owner_id: int, authority_mode: NetSyncAuthorityMode) -> bool:
+	if authority_mode == NetSync.NetSyncAuthorityMode.SHARED:
+		return true
+	return from_id == 1 or from_id == owner_id
